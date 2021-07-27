eb41de85a79bb91acb2f37afe8fbee8138e2dee0
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2021-07-27 12:48:19 +0200

    add material ui thames

---
+++ material-UI-work-whit-thame/styles/HomeStyles.js
@@ -0,0 +1,70 @@
+import sizes from "../../../root-styles/BreakPoints";
+import colors from "../../../root-styles/Colors";
+import fonts from "../../../root-styles/Fonts";
+
+const drawerWidth = 240;
+
+const styles = (theme) => ({
+  root: {
+    display: "flex",
+    height: "200vh",
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  widthbar: { border: "1px solid blue", backgroundColor: "transparent" },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    // display: "none",
+    border: "1px solid red",
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+  myclass: {
+    backgroundColor: "white",
+  },
+});
+export default styles;
